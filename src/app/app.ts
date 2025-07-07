c214e9e5118f0aef04d03f04592d90505c0baf6f
PongoGergo <gergopongo@gmail.com>
2025-07-07 13:09:33 +0200

    standalone

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,7 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
