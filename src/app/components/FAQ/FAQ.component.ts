08b6af913ce0a397a9488cb037f559987e6126f5
PongoGergo <gergopongo@gmail.com>
2025-07-14 13:24:02 +0200

    FAQ content

--- src/app/components/FAQ/FAQ.component.ts
+++ src/app/components/FAQ/FAQ.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AccordionModule } from 'primeng/accordion';
 
 @Component({
   selector: 'app-FAQ',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, AccordionModule],
   templateUrl: './FAQ.component.html',
   styleUrls: ['./FAQ.component.css'],
 })
