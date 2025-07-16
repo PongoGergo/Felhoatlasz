c7c9ad1af142a80321176b2e6a9964a8c9ae8b3d
PongoGergo <gergopongo@gmail.com>
2025-07-16 11:06:41 +0200

    scroll top fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -12,10 +12,41 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 export class App {
   protected title = 'Felhőatlasz';
   mobileNavCollapse = true;
+  isShow: boolean = false;
+  topPosToStartShowing = 100;
 
   constructor() {}
 
   toggleNavBar() {
     this.mobileNavCollapse = !this.mobileNavCollapse;
+  }
+
+  @HostListener('window:scroll')
+  checkScroll() {
+    // window의 scroll top
+    // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. window.pageYOffset is not supported below IE 9.
+
+    const scrollPosition =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+
+    console.log('[scroll]', scrollPosition);
+
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+    console.log('[isShow]', this.isShow);
+  }
+
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
   }
 }
